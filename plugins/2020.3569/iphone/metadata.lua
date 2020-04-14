5a0ab6d4f44773527a9c728e4d1e3ae9d7c2ba5b
Vlad Shcherban <vlad@coronalabs.com>
2020-04-14 00:05:46 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDTwitterMoPubAdapter', 'Avid-mopub-3.6.1', },
+		frameworks = { 'MPUBMoatMobileAppKit', 'MoPub', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
